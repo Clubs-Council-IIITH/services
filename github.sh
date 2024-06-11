ea46ae6e25822813b6770d8b8efbdc365d523f56
Bhav Beri <bhavberi@gmail.com>
2024-06-11 12:10:26 +0530

    Added github pull/push script

---
+++ github.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Check if exactly one argument is provided
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 [pull|push]"
+    exit 1
+fi
+
+case "$1" in
+    pull)
+        git submodule foreach "git pull origin master"
+        git pull origin master
+        ;;
+    push)
+        git submodule foreach "git push origin master"
+        git push origin master
+        ;;
+    *)
+        echo "Error: The parameter must be 'pull', or 'push'"
+        exit 1
+        ;;
+esac
