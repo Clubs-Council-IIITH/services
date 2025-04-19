d6c4370aee0bc5fe34829ecd28997135af3adabb
notpua <atp@tutamail.com>
2025-04-19 15:17:59 +0530

    config: add custom logger config

---
+++ config/logging_config.py
@@ -0,0 +1,19 @@
+import logging
+import datetime
+import pytz
+
+class ISTFormatter(logging.Formatter):
+    """Custom formatter that converts timestamps to IST timezone"""
+    
+    def formatTime(self, record, datefmt=None):
+        dt = datetime.datetime.fromtimestamp(record.created)
+        
+        ist_tz = pytz.timezone('Asia/Kolkata')
+        dt = dt.astimezone(ist_tz)
+        
+        if datefmt:
+            s = dt.strftime(datefmt)
+        else:
+            s = dt.strftime("[%Y-%m-%d %H:%M:%S]")
+        
+        return s
